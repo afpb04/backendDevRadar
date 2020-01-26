657ce3c01bf30bc24f9a9977110be30ae6f3a589
Antonio <afpb04@hotmail.com>
2020-01-26 10:23:42 -0300

    backend da aplicação concluido

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,31 @@
+const Dev = require('../models/Dev');
+const ParseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = ParseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+
+                },
+            },
+        });
+
+        console.log(techsArray);
+
+        return response.json({ devs });
+
+    }
+}
