c00f03f1f592dee5e3e8a6d0fe1dd31d0f77f731
Antonio <afpb04@hotmail.com>
2020-01-26 16:34:24 -0300

    deprecatonWarning fix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
+const cors = require('cors');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-iffww.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
-
+app.use(cors({
+  //  origin: 'http://localhoste:3000'
+}));
 app.use(express.json());
 app.use(routes);
 
