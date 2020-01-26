86fd0fecb43f9a94394d8fba59822cdc9cb73fd6
Antonio <afpb04@hotmail.com>
2020-01-26 20:05:46 -0300

    Configuração para deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-iffww.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -16,5 +16,5 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(routes);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 //app.listen(3333);
